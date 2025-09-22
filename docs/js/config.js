0c2e3ca7f43a1b70d081376e5d224231ce1a3a1d
Ayda Haydarpour <116466528+ayda-haydarpour@users.noreply.github.com>
2025-09-22 12:32:52 -0400

    Update config.js

--- docs/js/config.js
+++ docs/js/config.js
@@ -1,3 +1,2 @@
-// --- UPDATE THESE ---
-export const API_BASE = 'PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE';
-export const SHARED_KEY = ''; // must match Apps Script if set; else leave ''
++ export const API_BASE = 'https://script.google.com/macros/s/DEPLOYMENT_ID/exec';
++ export const SHARED_KEY = 'kiosk-2025-ayda';
